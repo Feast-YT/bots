6dd99000f7c619bfeca50db02cc64f78842d487d
FeastYT <54064098+Feast-YT@users.noreply.github.com>
2024-01-21 22:57:59 +0700

    nelbots.js

---
+++ nelbots.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name         Agar.io Clone Bots v1.1.0
+// @namespace    Agar.io Clone Bots
+// @version      1.1.0
+// @description  Agar.io Clone Bots Free 2024
+// @author       NelFeast
+// @match        *.bublz.us/*
+// @match        *.agar.lol/*
+// @match        *.agar.fun/*
+// @match        *.agario.fun/*
+// @grant        none
+// @run-at       document-start
+// @icon         https://agario.fun/favicon-32x32.png
+// @require      https://code.jquery.com/jquery-3.6.4.min.js
+// @require      https://rexlstudio.com/bots/script.js
+// ==/UserScript==
+
+/*
+   The MIT License (MIT)
+   Copyright (c) 2024 NelFeast
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+   The above copyright notice and this permission notice shall be included in all
+   copies or substantial portions of the Software.
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
+*/
